ed42ccb28d9f3dbd68808418d3a042ed75c9ca61
Datdater <datnqse173081@fpt.edu.vn>
2025-06-05 22:27:26 +0700

    Reorganize import statements in MainRouter for improved readability
    - Moved Layout import to the correct position to enhance clarity in the import structure.

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -9,8 +9,8 @@ import Checkout from "@/pages/checkout/Checkout";
 import OrderCustomerPage from "@/pages/profile/order-customer";
 import Search from "@/pages/search/Search";
 import StorePage from "@/pages/store/StorePage";
-import Layout from "@/components/layouts/Layout";
 import UserLayout from "@/components/layouts/UserLayout";
+import Layout from "@/components/layouts/Layout";
 const MainRouter = () => {
     return (
         <BrowserRouter>
