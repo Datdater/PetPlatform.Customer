06ef9c6eb1ab8cc1b19b251e6ba2cb5a6ff4206d
Datdater <datnqse173081@fpt.edu.vn>
2025-06-05 22:37:49 +0700

    Update import paths in MainRouter for consistency with project structure

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -9,8 +9,8 @@ import Checkout from "@/pages/checkout/Checkout";
 import OrderCustomerPage from "@/pages/profile/order-customer";
 import Search from "@/pages/search/Search";
 import StorePage from "@/pages/store/StorePage";
-import Layout from "src/components/layouts/Layout";
-import UserLayout from "src/components/layouts/UserLayout";
+import Layout from "@/components/layouts/layout";
+import UserLayout from "@/components/layouts/userLayout";
 const MainRouter = () => {
     return (
         <BrowserRouter>
