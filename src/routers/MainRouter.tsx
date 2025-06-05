2f06c84d7df420b915bdde64856c4098ff79f104
Datdater <datnqse173081@fpt.edu.vn>
2025-06-05 22:25:59 +0700

    Reorganize imports in MainRouter for clarity and consistency
    - Moved Layout and UserLayout imports to the correct position for better readability.
    - Ensured consistent import structure across the MainRouter component.

--- src/routers/MainRouter.tsx
+++ src/routers/MainRouter.tsx
@@ -1,16 +1,16 @@
-import Layout from "@/components/layouts/Layout";
 import Home from "@/pages/home/Home";
 import ProductDetail from "@/pages/product/ProductDetail";
 import ProfileCustomer from "@/pages/profile/profile-customer";
 import AddressCustomer from "@/pages/profile/address-customer";
 import { BrowserRouter, Route, Routes } from "react-router";
 import UserProvider from "@/store/contexts/UserContext";
-import UserLayout from "@/components/layouts/UserLayout";
 import CartPage from "@/pages/cart/Cart";
 import Checkout from "@/pages/checkout/Checkout";
 import OrderCustomerPage from "@/pages/profile/order-customer";
 import Search from "@/pages/search/Search";
 import StorePage from "@/pages/store/StorePage";
+import Layout from "@/components/layouts/Layout";
+import UserLayout from "@/components/layouts/UserLayout";
 const MainRouter = () => {
     return (
         <BrowserRouter>
